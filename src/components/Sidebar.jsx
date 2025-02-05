d1f3f6ffac53e496dcfb4555a1774540b2abf5a1
francescapar <ftparodi29@gmail.com>
2025-02-04 22:57:19 -0500

    Test

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ import { useState, useCallback } from "react"
  */
 export default function Sidebar() {
   let [newMenuItem, setNewMenuItem] = useState("")
-  // TODO: 2 Using a state hook, maintain the current menu items as an array state.
+  // TODO: 2 Using a state hook, maintain the current menu items as an array state..
   // let [menuItems, setMenuItems] = useState(initialMenuItems)
   let [filter, setFilter] = useState("")
   // Adds a single string passed in as parameter to the state element
